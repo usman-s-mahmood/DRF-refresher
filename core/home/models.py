b05fc2eae135dd9cca2cf2cb6f5ced3ff85cd894
0nyx-Fr0st <usmanshahid027@outlook.com>
2025-03-04 17:57:03 +0500

    did some working in serializer validation

--- core/home/models.py
+++ core/home/models.py
@@ -2,8 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class Color(models.Model):
+    color_name = models.CharField(max_length=120)
+    
+    def __str__(self) -> str:
+        return self.color_name
+
 class Person(models.Model):
+    color = models.ForeignKey(
+        Color,
+        on_delete=models.CASCADE,
+        related_name='color_detail',
+        blank=True,
+        null=True
+    )
     name = models.CharField(max_length=130)
     age = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
-
+    
+    def __str__(self) -> str:
+        return f'{self.name} | {self.created_on.date()}'
--- core/home/serializers.py
+++ core/home/serializers.py
@@ -2,13 +2,47 @@
 from . import models
 from rest_framework import serializers
 
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Color
+        fields = ['color_name']
+
 class PeopleSerializer(serializers.ModelSerializer):
-    
+    color = ColorSerializer() # it will only show color_name field based on the ColorSerializer
+    color_info = serializers.SerializerMethodField()
     class Meta:
         model = models.Person
-        fields = [
-            'id',
-            'name',
-            'age',
-            'created_on'
-        ]
+        # fields = [
+        #     'id',
+        #     'name',
+        #     'age',
+        #     'created_on'
+        # ]
+        fields = '__all__'
+        # depth = 1 # for populating foreign key, value can be high if more fields are required
+        
+    def get_color_info(self, obj):
+        # color_obj = models.Color.objects.get(id = obj.color.id)
+        # return {
+        #     'color_name': color_obj.color_name,
+        #     'hex_code': '#000'
+        # }
+        if obj.color != None:
+            return {
+                'color_info_name': models.Color.objects.get(id = obj.color.id).color_name,
+                'hex_code': '#000'
+            }
+        print(f'color ID found: {obj.color != None }')
+        return "Not Found"
+        
+    def validate_name(self, name):
+        special_chars = '''!@#$%^&*()_+-=[]}{:"|;'\\<>?,./<'''
+        if any(c in special_chars for c in name):
+            raise serializers.ValidationError("Name Must Not contain any special characters!")
+        return name
+        
+    def validate(self, data): # for single field validate_FieldName can be used
+        if data["age"] < 18:
+            raise serializers.ValidationError("Age Must be >= 18")
+        return data # this returns valid data to the serializer
